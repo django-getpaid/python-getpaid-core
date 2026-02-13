cc2637a401e9e74d07da1ad7990b46a9e0a94ccb
Dominik Kozaczko <dominik@kozaczko.info>
2026-02-13 19:54:21 +0100

    docs: update Sphinx config, deps, and RTD config for getpaid-core

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,29 @@
-"""Sphinx configuration."""
-project = "Getpaid Core"
+"""Sphinx configuration for getpaid-core."""
+
+project = "getpaid-core"
 author = "Dominik Kozaczko"
-copyright = "2022, Dominik Kozaczko"
+copyright = "2022-2026, Dominik Kozaczko"
+
 extensions = [
     "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
     "sphinx.ext.napoleon",
-    "sphinx_click",
+    "sphinx.ext.intersphinx",
     "myst_parser",
 ]
+
 autodoc_typehints = "description"
+autodoc_member_order = "bysource"
+autosummary_generate = True
+
 html_theme = "furo"
+html_title = "getpaid-core"
+
+myst_enable_extensions = [
+    "colon_fence",
+    "fieldlist",
+]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+}
