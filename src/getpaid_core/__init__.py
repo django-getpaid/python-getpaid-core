6d4bbc391f2d403b805d5bcaf8f0e75dcfcece6d
Dominik Kozaczko <dominik@kozaczko.info>
2026-02-18 02:31:57 +0100

    build(version): bump to 3.0.0a2, update cross-deps and PyPI metadata

--- src/getpaid_core/__init__.py
+++ src/getpaid_core/__init__.py
@@ -1,6 +1,6 @@
 """Getpaid Core -- framework-agnostic payment processing."""
 
-__version__ = "0.1.1"
+__version__ = "3.0.0a2"
 
 from getpaid_core.enums import BackendMethod
 from getpaid_core.enums import ConfirmationMethod
