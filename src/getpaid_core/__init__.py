c5df66bafb8c334d28a57da7b45f89c02ce48a56
Dominik Kozaczko <dominik@kozaczko.info>
2026-06-04 00:57:41 +0200

    bump: version 3.0.0a4 → 3.0.0 for stable release

--- src/getpaid_core/__init__.py
+++ src/getpaid_core/__init__.py
@@ -1,6 +1,6 @@
 """Getpaid Core -- framework-agnostic payment processing."""
 
-__version__ = "3.0.0a4"
+__version__ = "3.0.0"
 
 from getpaid_core.enums import BackendMethod
 from getpaid_core.enums import ConfirmationMethod
